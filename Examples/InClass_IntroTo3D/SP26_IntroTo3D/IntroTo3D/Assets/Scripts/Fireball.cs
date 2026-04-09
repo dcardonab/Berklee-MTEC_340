886710aa22aa82c36d1f44269748d1de9f46a6f5
David <dcbapc@gmail.com>
2026-04-09 11:42:08 -0400

    Added fireball

---
+++ Examples/InClass_IntroTo3D/SP26_IntroTo3D/IntroTo3D/Assets/Scripts/Fireball.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private float _speed = 15.0f;
+    [SerializeField] int _damage = 1;
+    
+    void Update()
+    {
+        transform.Translate(0.0f, 0.0f, _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- Examples/InClass_IntroTo3D/SP26_IntroTo3D/IntroTo3D/Assets/Scripts/Wandering.cs
+++ Examples/InClass_IntroTo3D/SP26_IntroTo3D/IntroTo3D/Assets/Scripts/Wandering.cs
@@ -8,12 +8,14 @@ public class Wandering : MonoBehaviour
     [SerializeField] private float _sphereRadius = 0.75f;
 
     private bool _isAlive;
-
     public bool IsAlive
     {
         get => _isAlive;
         set => _isAlive = value;
     }
+
+    [SerializeField] private GameObject _fireballPrefab;
+    [HideInInspector] public GameObject Fireball;
     
     void Start()
     {
@@ -31,8 +33,20 @@ public class Wandering : MonoBehaviour
 
             if (Physics.SphereCast(ray, _sphereRadius, out RaycastHit hit))
             {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    if (!Fireball)
+                    {
+                        Fireball = Instantiate(
+                            _fireballPrefab,    // GameObject to instantiate
+                            transform.TransformPoint(Vector3.forward * 1.5f),
+                            transform.rotation
+                        );
+                    }
+                }
+                
                 // When close enough to an obstacle or entity, rotate
-                if (hit.distance < _obstacleRange)
+                else if (hit.distance < _obstacleRange && !hit.transform.CompareTag("Fireball"))
                 {
                     float theta = Random.Range(-135.0f, 135.0f);
                     transform.Rotate(0.0f, theta, 0.0f);
